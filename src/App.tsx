95e39189d17ef569eda920822c8d4c04267b8e20
Chandra Bhan Singh <bhanumeister@gmail.com>
2024-08-06 11:37:26 +0530

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import options from './data';
-// import axios from "axios";
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
@@ -10,7 +10,7 @@ import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
 
 const App = () => {
   const [selected, setSelected] = useState([]);
-  // const [stateData, setData] = useState([]);
+  const [stateData, setData] = useState([]);
   
 
   // useEffect(() => {
@@ -20,18 +20,14 @@ const App = () => {
   //     .catch(error => console.error(error));
   // }, []);
 
-  // useEffect(() => {
-  //   // fetch('https://jsonplaceholder.typicode.com/todos')
-  //   //   .then(response => response.json())
-  //   //   .then(json => setData(json))
-  //   //   .catch(error => console.error(error));
-  //   getData("https://jsonplaceholder.typicode.com/todos");
-  // }, []);
+  useEffect(() => {
+    getData('https://jsonplaceholder.typicode.com/todos');
+  }, []);
 
-  // const getData = async (url) => {
-  //   try {const response = await axios.get(url);
-  //     setData(response);
-  //     console.log(response);} catch (error) {console.log(error);}};
+  const getData = async (url) => {
+    try {const response = await axios.get(url);
+       setData(response.data);
+       console.log(response.data);} catch (error) {console.log(error);}};
 
 
   return (
@@ -40,7 +36,7 @@ const App = () => {
       <div className="Title-Subtitle">Front End Codding Challenge</div>   
      </div>
     
-    <label>Select State of USA</label>
+    <label>Select State of USA From Json</label>
     
     <Typeahead className="search-box"
       id="serach"
@@ -49,22 +45,15 @@ const App = () => {
       placeholder="States of USA..."
       selected={selected}
     />
- {/* <Typeahead className="search-box"
-      id="serach_api"
-      labelKey="title"      
-      options={stateData}
-      onChange={setData}
-      placeholder="Select user ..."
-      selected={selected}
-    /> */}
- {/* <Typeahead className="search-box"
+<label>Select User with API </label>
+ <Typeahead className="search-box"
       id="serach_api"
       labelKey="title"   
-      onChange={setSelected}   
+      onChange={setData}   
       options={stateData}
       placeholder="select user..."
-      selected={selected}
-    /> */}
+      // selected={stateData}
+    />
 
 </div>
   );
